1d1a117eb8681eee90f76881980aba7f4977fc2f
abderrazzaq-laanoui <laanoui.fst@uhp.ac.ma>
2021-05-23 13:11:46 +0100

    adding modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,8 +3,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { UserModule } from './user/user.module';
 import { DocumentModule } from './document/document.module';
+import { SemestreModule } from './semestre/semestre.module';
+import { SemesterController } from './semester/semester.controller';
+import { ModuleModule } from './module/module.module';
+import { RapportModule } from './rapport/rapport.module';
+import { DemandeModule } from './demande/demande.module';
+import { EvenementModule } from './evenement/evenement.module';
+import { NoteModule } from './note/note.module';
+import { MatiereModule } from './matiere/matiere.module';
+import { CalendrierModule } from './calendrier/calendrier.module';
+import { AbsenceModule } from './absence/absence.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), UserModule, DocumentModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), UserModule, DocumentModule, SemestreModule, ModuleModule, RapportModule, DemandeModule, EvenementModule, NoteModule, MatiereModule, CalendrierModule, AbsenceModule],
+  controllers: [SemesterController],
 })
 export class AppModule {}
